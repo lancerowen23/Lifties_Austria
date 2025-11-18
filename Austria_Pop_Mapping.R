638e58784769f670eefafa902af82f3603e7c1c4
Lance R. Owen, PhD <44533523+lancerowen23@users.noreply.github.com>
2025-11-18 10:54:07 -0700

    Script edits and data additions

--- Austria_Pop_Mapping.R
+++ Austria_Pop_Mapping.R
@@ -1,4 +1,6 @@
-##Mapping Elderly Populations
+##Mapping Elderly Populations // Austria 
+#All data 2024
+#Spatial and Pop Data from Statistik Austria
 
 library(readr)
 library(sf)
@@ -6,146 +8,79 @@ library(dplyr)
 library(leaflet)
 library(RColorBrewer)
 
+#GeoJSON Import 
+
 #Bezirke
 bez_url <- "https://raw.githubusercontent.com/lancerowen23/Lifties_Austria/main/austria_bezirke_simplified.geojson"  
 bezirke <- st_read(bez_url, quiet = FALSE)
-
-bez_csv <- read_csv('Desktop/Lifties_Austria/pop_75andUp_bez.csv')
-View(bez_csv)
-
-#join data
-bez_merge <- merge(bezirke, bez_csv, by.x="g_id", by.y="id")
-
-#map
-
-value_column <- "percent"
-
-pal <- colorNumeric(
-  palette = "YlOrRd",
-  domain = bez_merge[[value_column]],
-  na.color = "#cccccc"
-)
-
-# 4. Build the interactive leaflet map // Bezirke
-leaflet(bez_merge) %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addPolygons(
-    fillColor = ~pal(get(value_column)),
-    weight = 1,
-    opacity = 1,
-    color = "white",
-    dashArray = "1",
-    fillOpacity = 0.7,
-    
-    # highlight on hover
-    highlight = highlightOptions(
-      weight = 2,
-      color = "#666",
-      fillOpacity = 0.9,
-      bringToFront = TRUE),
-    
-    # popup information
-    popup = ~paste0(
-      "<strong>", name, "</strong><br/>",
-      get(value_column), "% age 75 and older."),
-    
-  #   # label on hover
-  #   label = ~paste0(percent, ": ", get(value_column)),
-  #   labelOptions = labelOptions(
-  #     style = list("font-weight" = "normal", padding = "3px 8px"),
-  #     textsize = "13px",
-  #     direction = "auto")) 
-  )%>%
-  addLegend(
-    pal = pal,
-    values = bez_merge[[value_column]],
-    opacity = 0.8,
-    title = "Pop. | % 75+",
-    position = "bottomright"
-  )
-
-#mapping pop number
-
-value_column <- "abs"
-
-pal <- colorNumeric(
-  palette = "YlOrRd",
-  domain = bez_merge[[value_column]],
-  na.color = "#cccccc"
-)
-
-# 4. Build the interactive leaflet map // Bezirke
-leaflet(bez_merge) %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addPolygons(
-    fillColor = ~pal(get(value_column)),
-    weight = 1,
-    opacity = 1,
-    color = "white",
-    dashArray = "1",
-    fillOpacity = 0.7,
-    
-    # highlight on hover
-    highlight = highlightOptions(
-      weight = 2,
-      color = "#666",
-      fillOpacity = 0.9,
-      bringToFront = TRUE),
-    
-    # popup information
-    popup = ~paste0(
-      "<strong>", name, "</strong><br/>",
-      get(value_column), " inhabitants age 75 and older."),
-    
-    #   # label on hover
-    #   label = ~paste0(percent, ": ", get(value_column)),
-    #   labelOptions = labelOptions(
-    #     style = list("font-weight" = "normal", padding = "3px 8px"),
-    #     textsize = "13px",
-    #     direction = "auto")) 
-  )%>%
-  addLegend(
-    pal = pal,
-    values = bez_merge[[value_column]],
-    opacity = 0.8,
-    title = "Pop.75+",
-    position = "bottomright"
-  )
-
-
 #Gemeinden
 gem_url <- "https://raw.githubusercontent.com/lancerowen23/Lifties_Austria/main/austria_gemeinden_simplified.geojson"  
 gemeinden <- st_read(gem_url, quiet = FALSE)
 
-gem_csv <- read.csv('Desktop/Lifties_Austria/pop_75andUp_gem.csv', encoding = "UTF-8")
+#Pop Files Import
+
+#total population
+bez_pop_total <- read_csv('Desktop/Lifties_Austria/pop_total_bez.csv')
+gem_pop_total <- read.csv('Desktop/Lifties_Austria/pop_total_gem.csv', encoding = "UTF-8")
+#population 60-74 years
+bez_pop_60to74 <- read_csv('Desktop/Lifties_Austria/pop_60to74_bez.csv')
+gem_pop_60to74 <- read.csv('Desktop/Lifties_Austria/pop_60to74_gem.csv', encoding = "UTF-8")
+#population 75+ years
+bez_pop_75andUp <- read_csv('Desktop/Lifties_Austria/pop_75andUp_bez.csv')
+gem_pop_75andUp <- read.csv('Desktop/Lifties_Austria/pop_75andUp_gem.csv', encoding = "UTF-8")
 
 #join data
-gem_merge <- merge(gemeinden, gem_csv, by.x="g_id", by.y="id")
+#bezirke
+bez_df <- merge(bezirke, bez_pop_total, by.x="g_id", by.y="id")
+bez_df <- merge(bez_df, bez_pop_60to74, by.x="g_id", by.y="id")
+bez_df <- merge(bez_df, bez_pop_75andUp, by.x="g_id", by.y="id")
+bez_final_df <- bez_df %>% 
+  select(g_id, name, total_pop, abs.x, percent.x, abs.y, percent.y, geometry) %>% 
+  rename(id = g_id,
+         pop_60to74 = abs.x,
+         percent_60to74 = percent.x,
+         pop_75andUp = abs.y,
+         percent_75andUp = percent.y)
+View(bez_final_df)
 
-# 2. Choose a numeric column to visualize
-# Replace "my_value" with the column from your CSV you want to map
-value_column <- "percent"
+#gemeinden
+gem_df <- merge(gemeinden, gem_pop_total, by.x="g_id", by.y="id")
+gem_df <- merge(gem_df, gem_pop_60to74, by.x="g_id", by.y="id")
+gem_df <- merge(gem_df, gem_pop_75andUp, by.x="g_id", by.y="id")
+gem_final_df <- gem_df %>% 
+  select(g_id, name, total_pop, abs.x, percent.x, abs.y, percent.y, geometry) %>% 
+  rename(id = g_id,
+         pop_60to74 = abs.x,
+         percent_60to74 = percent.x,
+         pop_75andUp = abs.y,
+         percent_75andUp = percent.y)
+View(gem_final_df)
 
-# 3. Create a color palette for choropleth
+#mapping with Leaflet
+
+#bezirke 
+
+value_column <- "percent_75andUp"
+
+#Define color palette
 pal <- colorNumeric(
   palette = "YlOrRd",
-  domain = gem_merge[[value_column]],
-  na.color = "#cccccc"
-)
+  domain = bez_final_df[[value_column]],
+  na.color = "#cccccc")
 
-# 4. Build the interactive leaflet map // Gemeinden
-leaflet(gem_merge) %>%
+#Build the interactive leaflet map
+leaflet(bez_final_df) %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(
     fillColor = ~pal(get(value_column)),
     weight = 1,
     opacity = 1,
     color = "white",
-    dashArray = "3",
+    dashArray = "1",
     fillOpacity = 0.7,
     
     # highlight on hover
-    highlight = highlightOptions(
+    highlightOptions = highlightOptions(
       weight = 2,
       color = "#666",
       fillOpacity = 0.9,
@@ -156,46 +91,38 @@ leaflet(gem_merge) %>%
     popup = ~paste0(
       "<strong>", name, "</strong><br/>",
       get(value_column), "% age 75 and older."
-    ),
-    
-    # # label on hover
-    # label = ~paste0(percent, ": ", get(value_column)),
-    # labelOptions = labelOptions(
-    #   style = list("font-weight" = "normal", padding = "3px 8px"),
-    #   textsize = "13px",
-    #   direction = "auto"
-    # )
-  ) %>%
+    )) %>%
   addLegend(
     pal = pal,
-    values = gem_merge[[value_column]],
-    opacity = 0.7,
-    title = "Pop. | % 75+",
+    values = bez_final_df[[value_column]],
+    opacity = 0.8,
+    title = "Pop. 75+",
     position = "bottomright"
   )
 
-## Pop Numbers
-value_column <- "abs"
+#Gemeinden
 
-# 3. Create a color palette for choropleth
+value_column <- "percent_75andUp"
+
+#Define color palette
 pal <- colorNumeric(
   palette = "YlOrRd",
-  domain = gem_merge[[value_column]],
-  na.color = "#cccccc"
-)
+  domain = gem_final_df[[value_column]],
+  na.color = "#cccccc")
 
-leaflet(gem_merge) %>%
+#Build the interactive leaflet map
+leaflet(gem_final_df) %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(
     fillColor = ~pal(get(value_column)),
     weight = 1,
     opacity = 1,
     color = "white",
-    dashArray = "3",
+    dashArray = "1",
     fillOpacity = 0.7,
     
     # highlight on hover
-    highlight = highlightOptions(
+    highlightOptions = highlightOptions(
       weight = 2,
       color = "#666",
       fillOpacity = 0.9,
@@ -205,22 +132,12 @@ leaflet(gem_merge) %>%
     # popup information
     popup = ~paste0(
       "<strong>", name, "</strong><br/>",
-      get(value_column), " inhabitants age 75+."
-    ),
-    
-    # # label on hover
-    # label = ~paste0(percent, ": ", get(value_column)),
-    # labelOptions = labelOptions(
-    #   style = list("font-weight" = "normal", padding = "3px 8px"),
-    #   textsize = "13px",
-    #   direction = "auto"
-    # )
-  ) %>%
+      get(value_column), "% age 75 and older."
+    )) %>%
   addLegend(
     pal = pal,
-    values = gem_merge[[value_column]],
-    opacity = 0.7,
-    title = "Pop. | % 75+",
+    values = gem_final_df[[value_column]],
+    opacity = 0.8,
+    title = "Pop. 75+",
     position = "bottomright"
   )
-
