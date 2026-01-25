b85af707d28a2af229ea6a297700266d781c4432
Lance R. Owen, PhD <44533523+lancerowen23@users.noreply.github.com>
2026-01-24 21:31:09 -0700

    updates

--- Income_Data_Raw/Austria_Stat_Analysis.R
+++ Income_Data_Raw/Austria_Stat_Analysis.R
@@ -5,3 +5,4 @@ gem_path <- "Desktop/Lifties_Austria/Final_Data_Cleaned/Combined/gemeinden_final
 gem_customers <- st_read(gem_path, quiet = FALSE)
 gem_customers <- gem_customers %>%
   mutate(address_count = if_else(is.na(address_count), 0L, address_count))
+
